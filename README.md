d70aec95bc8470667d210c468f6f783510d0530f
Jiayang Miao <jiayang.miao@gmail.com>
2016-11-01 02:39:38 -0700

    Formatting for output xlsx file is done

--- README.md
+++ README.md
@@ -8,5 +8,3 @@ It will group the songs in CDs with associated information related to each CD in
 
 ## How to Use
 To get started, log in into game (you do need an account), open the page of audio room (http://imas.gree-apps.net/app/index.php/audio_room), and save the webpage as "ml.html". Put it in same directory as the python script.
-
-Create a copy of the provided MLMusicInfoTemplate.xlsx and rename it as MLMusicInfo.xlsx. The script will write output into this file. Once you are done you are ready to run the script. 
--- mlparser.py
+++ mlparser.py
@@ -1,16 +1,30 @@
 # -*- coding: utf-8 -*-
 
+import os.path
+from sys import exit
+
 import lxml.html as html
 from lxml import etree
 import json
 
-print("Starting the Million Live Audio Room Parser...")
-print("Run this program in directory with html file of")
-print("URL:http://imas.gree-apps.net/app/index.php/audio_room")
-print("renamed to ml.html. ")
+import xlsxwriter
 
-# Parsing the HTML
+print("Starting the Million Live Audio Room Parser...")
+
+# Check for input file
+
+if os.path.exists("ml.html") != True:
+    print("Error: Cannot find ml.html.")
+    print("To get started properly, save the webpage of")
+    print("URL:http://imas.gree-apps.net/app/index.php/audio_room")
+    print("as ml.html and put it in same directory as the script.")
+    print("Exiting the program ...")
+    exit()
+
+# Parse the HTML
+
 parser = etree.HTMLParser(encoding='utf-8')
+print("Reading the file ...")
 tree = etree.parse('ml.html', parser)
 
 # The parser will return a list of nodes with 25 elements
@@ -19,36 +33,58 @@ tree = etree.parse('ml.html', parser)
 scriptNodes = tree.xpath('//script')
 scriptNode = scriptNodes[len(scriptNodes)-1]
 
-# In order to let json read this string
-# need to cut out "var albums = " and ";" at the two ends
+# Cut out "var albums = " and ";" at the two ends to get json string
 
 scriptText = scriptNode.text
-
 startIdentifier = 'var albums = '
 startIndex = scriptText.find(startIdentifier) + len(startIdentifier)
 endIndex = (len(scriptText) - scriptText.find(";")) * -1
-
 scriptText = scriptText[startIndex:endIndex]
 
-
-
-
-def byteify(input):
-    if isinstance(input, dict):
-        return {byteify(key): byteify(value)
-                for key, value in input.iteritems()}
-    elif isinstance(input, list):
-        return [byteify(element) for element in input]
-    elif isinstance(input, unicode):
-        return input.encode('utf-8')
-    else:
-        return input
+# Parse the json string
 
 parsedJson = json.loads(scriptText)
-#parsedJsonUnicoded = byteify(parsedJson)
-#parsedJson = json.loads(scriptTextUnicoded, strict=False)
 
-with open('test.txt', encoding='utf-8', mode='w+') as f:
-    #f.write(scriptTextUnicoded[:500])
-#with open('test.txt', encoding='utf-8', mode='w+') as f:
-    f.write(json.dumps(parsedJson, indent = 4, sort_keys = True))
+for thisItem in parsedJson:
+    # Check if is live setlist (which we are not parsing)
+    if thisItem['is_live'] != 0:
+        continue
+
+    else: # Actual album!
+
+        print("Found %s" %thisItem['album_title'])
+
+# Create output file and basic formatting
+
+workbook = xlsxwriter.Workbook('MLMusicInfo.xlsx')
+worksheet = workbook.add_worksheet()
+defaultformat = workbook.add_format()
+defaultformat.set_border(1)
+worksheet.set_column(0, 1, 30)
+worksheet.set_column(2, 2, 40)
+worksheet.set_column(3, 3, 10)
+worksheet.set_column(4, 4, 13)
+worksheet.set_column(5, 5, 40)
+worksheet.set_column(6, 7, 45)
+worksheet.set_column(8, 8, 10)
+worksheet.set_column(9, 9, 30)
+
+worksheet.write(0, 0, 'Million Live! In game Audio Room info sheet')
+
+headerformat = workbook.add_format()
+headerformat.set_align('center')
+headerformat.set_bold
+worksheet.write(1, 0, 'Album Composer', headerformat)
+worksheet.write(1, 1, 'Album Lyricist', headerformat)
+worksheet.write(1, 2, 'Album Artist', headerformat)
+worksheet.write(1, 3, 'Album Cover', headerformat)
+worksheet.write(1, 4, 'Album Release', headerformat)
+worksheet.write(1, 5, 'Album Title', headerformat)
+worksheet.write(1, 6, 'Song Title', headerformat)
+worksheet.write(1, 7, 'Song Artist', headerformat)
+worksheet.write(1, 8, 'Song URL', headerformat)
+worksheet.write(1, 9, 'Song Credit', headerformat)
+
+
+workbook.close()
+
