09d62392815a555f74d73f9b8417e28eb41571bb
Jiayang Miao <jiayang.miao@gmail.com>
2016-11-15 17:34:10 -0800

    Chart finished.

--- mlparser.py
+++ mlparser.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import os.path
+from time import strftime
 from sys import exit
 
-import lxml.html as html
 from lxml import etree
 import json
 
@@ -12,7 +12,6 @@ import xlsxwriter
 print("Starting the Million Live Audio Room Parser...")
 
 # Check for input file
-
 if os.path.exists("ml.html") != True:
     print("Error: Cannot find ml.html.")
     print("To get started properly, save the webpage of")
@@ -22,13 +21,11 @@ if os.path.exists("ml.html") != True:
     exit()
 
 # Parse the HTML
+# The album info is stored in the last script block (24)
 
 parser = etree.HTMLParser(encoding='utf-8')
 print("Reading the file ...")
 tree = etree.parse('ml.html', parser)
-
-# The parser will return a list of nodes with 25 elements
-# The album info is stored in the last script block (24)
 
 scriptNodes = tree.xpath('//script')
 scriptNode = scriptNodes[len(scriptNodes)-1]
@@ -41,50 +38,96 @@ startIndex = scriptText.find(startIdentifier) + len(startIdentifier)
 endIndex = (len(scriptText) - scriptText.find(";")) * -1
 scriptText = scriptText[startIndex:endIndex]
 
-# Parse the json string
-
 parsedJson = json.loads(scriptText)
 
-for thisItem in parsedJson:
-    # Check if is live setlist (which we are not parsing)
-    if thisItem['is_live'] != 0:
-        continue
-
-    else: # Actual album!
-
-        print("Found %s" %thisItem['album_title'])
-
 # Create output file and basic formatting
-
-workbook = xlsxwriter.Workbook('MLMusicInfo.xlsx')
+dateString = strftime("%Y-%m-%d%H%M")
+workbook = xlsxwriter.Workbook('MLMusicInfo{}.xlsx'.format(dateString))
 worksheet = workbook.add_worksheet()
-defaultformat = workbook.add_format()
-defaultformat.set_border(1)
+
 worksheet.set_column(0, 1, 30)
 worksheet.set_column(2, 2, 40)
-worksheet.set_column(3, 3, 10)
+worksheet.set_column(3, 3, 12)
 worksheet.set_column(4, 4, 13)
 worksheet.set_column(5, 5, 40)
 worksheet.set_column(6, 7, 45)
 worksheet.set_column(8, 8, 10)
 worksheet.set_column(9, 9, 30)
 
-worksheet.write(0, 0, 'Million Live! In game Audio Room info sheet')
+headerFormat = workbook.add_format()
+headerFormat.set_align('center')
+headerFormat.set_align('vcenter')
+headerFormat.set_border(1)
+headerFormat.set_bold()
 
-headerformat = workbook.add_format()
-headerformat.set_align('center')
-headerformat.set_bold
-worksheet.write(1, 0, 'Album Composer', headerformat)
-worksheet.write(1, 1, 'Album Lyricist', headerformat)
-worksheet.write(1, 2, 'Album Artist', headerformat)
-worksheet.write(1, 3, 'Album Cover', headerformat)
-worksheet.write(1, 4, 'Album Release', headerformat)
-worksheet.write(1, 5, 'Album Title', headerformat)
-worksheet.write(1, 6, 'Song Title', headerformat)
-worksheet.write(1, 7, 'Song Artist', headerformat)
-worksheet.write(1, 8, 'Song URL', headerformat)
-worksheet.write(1, 9, 'Song Credit', headerformat)
+defaultFormat = workbook.add_format()
+defaultFormat.set_align('center')
+defaultFormat.set_align('vcenter')
+defaultFormat.set_text_wrap()
+defaultFormat.set_border(1)
 
+urlFormat = workbook.add_format()
+urlFormat.set_align('vcenter')
+urlFormat.set_border(1)
+
+importantFormat = workbook.add_format()
+importantFormat.set_align('center')
+importantFormat.set_align('vcenter')
+importantFormat.set_text_wrap()
+importantFormat.set_border(1)
+importantFormat.set_bold()
+
+worksheet.merge_range(0, 0, 0, 9, 'Million Live! in game Audio Room info sheet', headerFormat)
+worksheet.write(1, 0, 'Album Composer', headerFormat)
+worksheet.write(1, 1, 'Album Lyricist', headerFormat)
+worksheet.write(1, 2, 'Album Artist', headerFormat)
+worksheet.write(1, 3, 'Album Cover', headerFormat)
+worksheet.write(1, 4, 'Album Release', headerFormat)
+worksheet.write(1, 5, 'Album Title', headerFormat)
+worksheet.write(1, 6, 'Song Title', headerFormat)
+worksheet.write(1, 7, 'Song Artist', headerFormat)
+worksheet.write(1, 8, 'Song URL', headerFormat)
+worksheet.write(1, 9, 'Song Credit', headerFormat)
+
+# Counter recording which row to write to
+count = 2
+
+for thisItem in parsedJson:
+    # Check if is live setlist (we are only parsing CDs)
+    if thisItem['is_live'] == 1:
+        continue
+
+    else: # Actual album!
+        endCount = count + len(thisItem['records'])-1
+
+        # Writing the album info
+        if len(thisItem['records']) == 1:
+            worksheet.write(count, 0, thisItem['composer'], defaultFormat)
+            worksheet.write(count, 1, thisItem['lyricist'], defaultFormat)
+            worksheet.write(count, 2, thisItem['album_artist_name'].replace('<br />','、').replace('<br>','、'), importantFormat)
+            worksheet.write(count, 3, thisItem['jacket'], urlFormat)
+            worksheet.write(count, 4, thisItem['release_date'], defaultFormat)
+            worksheet.write(count, 5, thisItem['album_title'].replace('<br/>','\n').replace('<br>','\n'), importantFormat)
+        else:
+            worksheet.merge_range(count, 0, endCount, 0, thisItem['composer'], defaultFormat)
+            worksheet.merge_range(count, 1, endCount, 1, thisItem['lyricist'], defaultFormat)
+            worksheet.merge_range(count, 2, endCount, 2, thisItem['album_artist_name'].replace('<br />','、').replace('<br>','、'), importantFormat)
+            worksheet.merge_range(count, 3, endCount, 3, thisItem['jacket'], urlFormat)
+            worksheet.merge_range(count, 4, endCount, 4, thisItem['release_date'], defaultFormat)
+            worksheet.merge_range(count, 5, endCount, 5, thisItem['album_title'].replace('<br/>','\n').replace('<br>','\n'), importantFormat)
+
+        # Writing the track info
+        trackCount = count
+        for thisTrack in sorted(thisItem['records']):
+            thisTrackInfo = thisItem['records']['{}'.format(thisTrack)]
+            worksheet.write(trackCount, 6, thisTrackInfo['music_title'].replace('<small>',' ').replace('</small>',' ').replace('<br>',' '), importantFormat)
+            worksheet.write(trackCount, 7, thisTrackInfo['music_artist_name'], importantFormat)
+            worksheet.write(trackCount, 8, thisTrackInfo['music_src'], urlFormat)
+            worksheet.write(trackCount, 9, thisTrackInfo['credit'], defaultFormat)
+
+            trackCount += 1
+
+        count = endCount + 1
 
 workbook.close()
 
